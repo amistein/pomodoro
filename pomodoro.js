e653094db9edf055301c8226884bc0925b5546a4
Amrom Steinmetz <amroms@gmail.com>
2016-06-06 13:25:16 -0400

    update 6/6

--- pomodoro.js
+++ pomodoro.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
 	var work = 25;
 	var rest = 5;
+	var running = false;
+	var rest = false;
+	var obj1 = document.createElement("audio");
+  obj1.src="sound1.mp3";      
+ 	obj1.volume = 0.5;
+	
 	$(".work-minute").html(work);
 	$(".rest-minute").html(rest);
+	$("#timer").html(work + ":" + "00");
 
 	$("#work-inner .arrow-up").click(function() {
 		work++;
 		$(".work-minute").html(work);
+		$("#timer").html(work + ":" + "00");
 	});
 
 	$("#work-inner .arrow-down").click(function() {
-		if (work > 0) {
+		if (work > 1) {
 			work--;
 			$(".work-minute").html(work);
+			$("#timer").html(work + ":" + "00");
 		}
 	});
 
@@ -28,4 +37,37 @@ $(document).ready(function() {
 		}
 	});
 
+	$("#go").click(function() {
+		$("body").css("background-color", "green");
+		var wMinutes = parseInt($(".work-minute").html());
+		var rMinutes = parseInt($(".rest-minute").html());
+		timer(rMinutes * 60);
+		while (true) {
+			if (rest) break;
+		}
+		$("body").css("background-color", "red");
+		timer(wMinutes * 60);
+
+	});
+
+	function timer(seconds) {
+		running = true;
+		var sec, min;
+		var clock = setInterval(function() {
+			if (running) {
+				seconds--;
+				sec = seconds % 60;
+				sec = sec < 10 ? "0" + sec : sec;
+				min = Math.floor(seconds / 60);
+				$("#timer").html(min + ":" + sec);
+
+				if (seconds == 0) {
+					obj1.play();
+					clearInterval(clock);
+					running = false;
+					rest = true;
+				}
+			}
+		}, 1000);
+	}
 })
